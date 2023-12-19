a40342b1a46ebd67329ac86b8e4adfaea9fe16f5
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2023-12-19 12:34:10 +0600

    individual data get

---
+++ dist/db.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.books = void 0;
+exports.books = [
+    {
+        id: "fkak1kfld-hdfghhdfgh-gsfgsfg",
+        title: "The Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        id: "gkak1kfld-rdfghhdfgh-dsfgsfg",
+        title: "City of Glass",
+        author: "Paul Auster",
+    },
+    {
+        id: "fkak1kfld-hdfghhdfgh-gsfg52g",
+        title: "Awakening",
+        author: "Kate Chopin",
+    },
+    {
+        id: "gkak1kfld-rdfghhdfgh-dsfgs2g",
+        title: "Glass",
+        author: "Paul Auster",
+    },
+    {
+        id: "fkak1kfld-hdfghhdfgh-gsf36fg",
+        title: "The",
+        author: "Kate Chopin",
+    },
+    {
+        id: "gkak1kfld-rdfghhdfgh-dsf95fg",
+        title: "of",
+        author: "Paul Auster",
+    },
+];
